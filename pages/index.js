fc9a080c26366d1c22e4ad6443c566383a0d2c58
darad124 <daryjoe765@gmail.com>
2023-09-11 19:14:01 +0100

    good

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// pages/index.js
+import React from 'react';
+import HomepageDesktop from '../pages/homepage-desktop'; // adjust the path as needed
+
+export default function Home() {
+  return <HomepageDesktop />;
+}
